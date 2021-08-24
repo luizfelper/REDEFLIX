8678cb01455ab5594d9bdc477b3f93fbc59298f6
Luiz Felipe <luizfelper@gmail.com>
2021-08-23 22:15:26 -0300

    Criar tela de loading e estiliza-la 💤

--- src/App.js
+++ src/App.js
@@ -66,6 +66,12 @@ export default () => {
         Dados obtidos via API do site Themoviedb.org.
       </footer>
 
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando"></img>
+        </div>
+      }
+
     </div>
   );
 }
